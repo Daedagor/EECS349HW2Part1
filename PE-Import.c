204c0afb14c932446d93df267221928ecea7d992
Daedagor <48215679+Daedagor@users.noreply.github.com>
2019-10-12 10:35:21 -0400

    Update PE-Import.c

--- PE-Import.c
+++ PE-Import.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define DATA_SIZE 1000
 
 bool file_exists (char *filename){
   struct stat buffer;
-  return (stat (filename, &buffer)
+  return (stat (filename, &buffer);
 }
 
 int main(void){
-    if (file_exists("PE-1.txt")
-      
+  
+  char data[DATA_SIZE];
+  
+  FILE * fPtr;
+  
+  if (file_exists("PE-1.txt"){
+   fopen("PE-1.txt","a");
+       
+    }
+    else{
+    
+    }    
 }
